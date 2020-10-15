b5c05733508abccebf48748688073ac2dff0ba6f
Yoon Jae Kim <yjkim@kyj-mbp20.local>
2020-10-15 11:11:34 +0900

    There are some to be updated.

--- export_xls_to_db_REF_TBL.py
+++ export_xls_to_db_REF_TBL.py
@@ -11,6 +11,7 @@ import mysql.connector
 dbconn = mysql.connector(host="localhost", user="root", passwd="xxxx", database="OOOOO")
 
 # declare global variables
+global exist_error
 exist_error = False
 global exit_now
 exit_now = False
@@ -21,6 +22,7 @@ os.chdir('C:\\Users\\KKKKK\\Downloads\\DIR_NAME')
 # function to match location for data clensing
 def load_location_mapping():
     global exit_now
+    global exist_error
 
     # Get the excel file
     cur_date = datetime.datetime.now()
@@ -45,7 +47,7 @@ def load_location_mapping():
     except Exception as e:
         exist_error = True
         dbconn.rollback()
-        print(e..message)
+        print(e.message)
         exit()
 
     print( '>>>> 4. Insert new data in the TABLE_NM1 table')
@@ -104,12 +106,13 @@ def load_location_mapping():
             break
 
     cur_date = datetime.datetime.now()
-    print( '>>>> 5. Finish to insert new data in the TALBE_NM1 table'  + cur_date.strftime( '%Y/%m/%d %H:%M:%S' ))
+    print( '>>>> 5. Finish to insert new data in the TALBE_NM1 table'  + cur_date.strftime( '%Y/%m/%d %H:%M:%S' ) + 'Total count: ' + str(j) )
     # for statement end ==============================
 
-# function to clease the data in TABLE_NM1
+# function to cleanse the data in TABLE_NM1
 def clenansing_data_TABLE_NM1():
     global exit_now
+    global exist_error
 
     cur_date = datetime.datetime.now()
     print('>>>> 1. Cleansing data in TABLE_NM1 table'  + cur_date.strftime( '%Y/%m/%d %H:%M:%S' ))
@@ -128,11 +131,11 @@ def clenansing_data_TABLE_NM1():
             ind = ind + 1
             cursor = dbconn.cursor()
 
-            if rec[3] == NONE:
+            if rec[3] == None:
                 print( '*************** The row number where None occurs:' + str(ind) )
                 if rec[1] == 'AAAA':
                     query1 = "UPDATE TABLE_NM1 SET COL_NM4 = '' WHERE COL_NM1 = '" + rec[0] + "' AND COL_NM2 = '" + rec[2] + "'"
-                elif rec[1] = 'BBBB':
+                elif rec[1] == 'BBBB':
                     query1 = "UPDATE TABLE_NM1 SET COL_NM4 = '' WHERE COL_NM1 = '" + rec[0] + "' AND COL_NM2 = '" + rec[2] + "' AND COL_NM4 = 'CCCC'"
             # replace "\n" with "%"
             elif "\n" in rec[2]:
@@ -140,12 +143,12 @@ def clenansing_data_TABLE_NM1():
                 tmp_str = rec[2]
                 if rec[1] == 'AAAA':
                     query1 = "UPDATE TABLE_NM1 SET COL_NM4 = '" + rec[3] + "' WHERE COL_NM1 = '" + rec[0] + "' AND COL_NM2 = '" + tmp_str.replace( "\n", "%" ) + "'"
-                elif rec[1] = 'BBBB':
+                elif rec[1] == 'BBBB':
                     query1 = "UPDATE TABLE_NM1 SET COL_NM4 = '" + rec[3] + "' WHERE COL_NM1 = '" + rec[0] +  "' AND COL_NM2 = '" + tmp_str.replace( "\n", "%" ) + "' AND COL_NM4 = 'CCCC'"
             else:
                 if rec[1] == 'AAAA':
                     query1 = "UPDATE TABLE_NM1 SET COL_NM4 = '" + rec[3] + "' WHERE COL_NM1 = '" + rec[0] + "' AND COL_NM2 = '" + rec[2] + "'"
-                elif rec[1] = 'BBBB':
+                elif rec[1] == 'BBBB':
                     query1 = "UPDATE TABLE_NM1 SET COL_NM4 = '" + rec[3] + "' WHERE COL_NM1 = '" + rec[0] +  "' AND COL_NM2 = '" + rec[2] + "' AND COL_NM4 = 'CCCC'"
             
             print( "*****************" + str(ind) + " - " + query1)
@@ -168,7 +171,7 @@ sel_input = int( input() )
 if sel_input == 1:
     load_location_mapping()
 elif sel_input == 2:
-    load_os_type_mapping()
+    clenansing_data_TABLE_NM1()
 else:
     print( 'You typed the wrong number!! (Press 1 or 2)') 
     exit()
